8a6c4ee2b44a7241e1aa0050cd76f97e103da666
Aman <patelaman0241@gmail.com>
2026-01-21 22:40:48 +0530

    add country code in number

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const WHATSAPP_NUMBER = '9913413515'; // Placeholder number
+export const WHATSAPP_NUMBER = '+91 9913413515'; // Placeholder number
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}`;
